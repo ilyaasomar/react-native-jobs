5682881985bebbcbaeb775b269dfffe609e12798
ilyaas <ilyaascumar10@gmail.com>
2023-03-19 14:20:37 +0300

    poused 46min

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+import styles from "./screenheader.style";
+
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+  return (
+    <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode="cover"
+        style={styles.btnImg(dimension)}
+      />
+    </TouchableOpacity>
+  );
+};
+
+export default ScreenHeaderBtn;
